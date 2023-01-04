f636adbdc4183d1c239f4cfc9c11f08af6e64002
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-04 11:38:01 +0900

    add test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true
+                    uniqueness: { case_sensitive: false }
 
 end
