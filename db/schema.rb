a25a7652b9995336c8595fd69c9d63aff27c6627
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-04 14:16:30 +0900

    セキュアなパスワード

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_035552) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_050501) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_21_035552) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
