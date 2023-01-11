dd1703885192f3cef42c3706aad3eca5b5093932
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-11 10:42:46 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+    # respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+        # format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        # format.json { render :show, status: :created, location: @user }
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        # format.html { render :new, status: :unprocessable_entity }
+        # format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'new', status: :unprocessable_entity
       end
-    end
+    # end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
