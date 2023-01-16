057acc17c6dace5cbb9f474b3972277f668051e0
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-16 14:50:19 +0900

    login test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,27 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= user.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      raise # テストがパスすれば、この部分がテストされていないことがわかる TODO:9.31まで進んだのでこの続きから
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
@@ -20,5 +41,19 @@ module SessionsHelper
   def log_out
     reset_session
     @current_user = nil?
+  end
+
+  # 永続セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    forget(current_user)
+    reset_session
+    @current_user = nil
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_011318) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_022742) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_011318) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
