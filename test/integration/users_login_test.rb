050fc5dfe6ab484ce2a23cb3caa07aeebd4066c5
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-16 11:01:48 +0900

    test refactoring

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,53 +1,75 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
+
   def setup
     @user = users(:michael)
   end
+end
 
-  test "login with invalid information" do
-    get login_path # ログイン用のパスを開く
-    assert_template 'sessions/new' # 新しいセッションのフォームが正しく表示されたことを確認する
-    post login_path, params: { session: { email: " ", password: " " } } # わざと無効なparamsハッシュを使ってセッション用パスにPOSTする
-    assert_response :unprocessable_entity # 新しいセッションのフォームが正しいステータスを返し、再度表示されることを確認する
-    assert_template 'sessions/new'
-    assert_not flash.empty? # フラッシュメッセージが表示されることを確認する
-    get root_path # 別ページにいったん移動する
-    assert flash.empty? # 移動先のページでフラッシュメッセージが表示されていないことを確認する
-  end
+class InvalidPasswordTest < UsersLogin
 
-  test "login with valid information followed by logout" do
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } } # ログイン用のパスを開く# セッション用パスに有効な情報をPOSTする
-    assert is_logged_in?
-    assert_redirected_to @user # リダイレクト先が正しいかどうかチェック
-    follow_redirect! # ページに実際に移動する
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0 # ログアウト用リンクが表示されなくなったことを確認する
-    assert_select "a[href=?]", logout_path # ログアウト用リンクが表示されていることを確認する
-    assert_select "a[href=?]", user_path(@user) # プロフィール用リンクが表示されていることを確認する
-    delete logout_path
-    assert_not is_logged_in?
-    assert_response :see_other
-    assert_redirected_to root_url
-    follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
-  end
-
-  
-  test "login with valid email/invalid password" do
+  test "login path" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email:    "abc@example.com",
+  end
+
+  test "login with valid email/invalid password" do
+    post login_path, params: { session: { email:    @user.email,
                                           password: "invalid" } }
-    assert_response :unprocessable_entity
-    assert_template 'sessions/new'
-    post login_path, params: { session: { email: @user.email, password: "invalid" } }
     assert_not is_logged_in?
+    assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+end
+
+class ValidLogin < UsersLogin
+
+  def setup
+    super
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do
+    assert is_logged_in?
+    assert_redirected_to @user
+  end
+
+  test "redirect after login" do
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+end
+
+class Logout < ValidLogin
+
+  def setup
+    super
+    delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+
+  test "successful logout" do
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
