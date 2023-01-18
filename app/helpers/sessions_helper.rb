e9d84c81fd405721e9665ed507e199e7f77c596a
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-18 10:54:06 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,21 +14,13 @@ module SessionsHelper
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= user.find_by(id: user_id)
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      raise # テストがパスすれば、この部分がテストされていないことがわかる TODO:9.31まで進んだのでこの続きから
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
-    end
-  end
-
-  # 現在ログイン中のユーザーを返す（いる場合）
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
