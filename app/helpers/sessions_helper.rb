ee471f5ccb7b5390d47fd166843f4095ac1bcb22
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-20 11:32:37 +0900

    friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -59,4 +59,9 @@ module SessionsHelper
     reset_session
     @current_user = nil
   end
+
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
